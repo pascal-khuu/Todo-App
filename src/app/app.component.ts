fbbd2360b92586e332960a1429b045c658cc3843
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-08 17:32:51 +0200

    first commit objectif minimum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CheckboxControlValueAccessor } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-app-project';
+  
+   texte:any[] = [];
+   
+  //  constructor(){
+  //    this.texte= []
+  //    this.text='';
+  //  }
+ 
+
+  ajouterTexte( p:string){
+   this.texte.push(p);
+   console.log(this.texte);
+   
+  }
+
+  onSubmit(dataForm: any) {
+    console.log(dataForm);
+    console.log(dataForm.form.value.details);
+    this.ajouterTexte(dataForm.form.value.details);
+       
+  }
+
+  
 }
