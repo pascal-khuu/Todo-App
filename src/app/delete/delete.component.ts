5f8c37ee309d9bca221eaab7850e9590c5b76cf7
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-13 04:41:36 +0200

    functionality delete

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete.component.css']
 })
 export class DeleteComponent implements OnInit {
-
-  constructor() { }
+ public bool;
+ public buttonDelete;
+  constructor() { 
+    this.bool=true;
+    this.buttonDelete=true;
+  }
 
   ngOnInit(): void {
+  }
+  deleteAll(){
+    this.bool=false;
+    alert('delete all tasks');
   }
 
 }
